df8ea8c4731ad3ee032e803095184ad8c33079e8
Marco Roggerini <m.roggerini@reply.it>
2024-08-05 22:34:23 +0200

    remove output file

--- anagrammi/permutations.py
+++ anagrammi/permutations.py
@@ -1,18 +1,6 @@
 from itertools import permutations
 
-word = 'tilao'
-with open('parole.txt') as f:
-    parole = f.readlines()
+word = 'idmelrapsi'
+for i in permutations(word):
+    print(''.join(i))
 
-anagrammi = []
-
-for i in parole:
-    new = i.replace('\n', '')
-    anagrammi.append(''.join(sorted(new)))
-
-word = ''.join(sorted(word))
-
-for i, parola in enumerate(anagrammi):
-    if parola == word:
-        print(parole[i])
-        exit()
